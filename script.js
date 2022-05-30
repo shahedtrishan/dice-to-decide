5bf8fdcbbc8a5dc7ea1aca657b1f4147de702d1a
Shahed Trishan <shahedtrishan51@gmail.com>
2022-05-31 04:17:48 +0600

    finalized code

--- script.js
+++ script.js
@@ -21,3 +21,13 @@ var randomImageSource2 = "images/" + randomDiceImage2;
 var image2 = document.querySelectorAll("img")[1];
 
 image2.setAttribute("src", randomImageSource2);
+
+//result section
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 won!"
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 won!"
+} else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "It's a tie!"
+}
