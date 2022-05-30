b9100e74350a5781a98d72797aeab1d8ac6b7159
Shahed Trishan <shahedtrishan51@gmail.com>
2022-05-31 04:05:48 +0600

    new lines

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
 
 //for 2nd image
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
